d557ea49d36f259e80ac6c68111ef4132ce78ca3
root <root@ip-172-31-36-46.ap-south-1.compute.internal>
2023-10-19 16:23:10 +0000

    modify

--- s3.tf
+++ s3.tf
@@ -6,4 +6,9 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "abhay-g-bucket"  # Replace with your desired bucket name
   acl    = "private"               # You can set the bucket's access control here
 }
+resource "aws_s3_bucket" "my_bk" {
+  bucket = "abhay-g-bk"  # Replace with your desired bucket name
+  acl    = "private"               # You can set the bucket's access control here
+
+}
 
