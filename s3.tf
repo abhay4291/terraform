599247cf442de7d42ccad3ea5db088ebe9b3d437
root <root@ip-172-31-36-46.ap-south-1.compute.internal>
2023-10-19 12:57:42 +0000

    added new file

---
+++ s3.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "ap-south-1"  # Update this to your desired AWS region
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "abhay-g-bucket"  # Replace with your desired bucket name
+  acl    = "private"               # You can set the bucket's access control here
+}
+
